ca3ef8b23a574fad4e5579d0b68c0500db01692d
Adam377 <57754640+Adam377@users.noreply.github.com>
2020-08-26 14:01:34 +0100

    Test C# Code for GitHub

---
+++ GitHubTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitHubTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GitHub Test");
+            Console.ReadLine();
+        }
+    }
+}
